17da463080323a7318a0d8ea3b30d5327e22dbe4
youssefakhardid <youssefakhardid@gmail.com>
2021-04-28 00:02:08 +0000

    initial commit

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,142 @@
+import express from 'express';
+import User from '../models/userModel';
+import { getToken } from '../util';
+const router = express.Router();
+router.post('/signin', async (req, res) => {
+  const signinUser = await User.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  if (signinUser) {
+    res.send({
+      _id: signinUser.id,
+      name: signinUser.name,
+      email: signinUser.email,
+      isAdmin: signinUser.isAdmin,
+      token: getToken(signinUser),
+    });
+  } else {
+    res.status(401).send({ message: 'Invalid Email or Password.' });
+  }
+});
+router.get('/createadmin', async (req, res) => {
+    try {
+      const user = new User({
+        name: 'admin',
+        email: 'admin@gmail.com',
+        password: '****',
+        isAdmin: true,
+      });
+      const newUser = await user.save();
+      res.send(newUser);
+    } catch (error) {
+      res.send({ message: error.message });
+    }
+  });
+  router.post('/register', async (req, res) => {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    const newUser = await user.save();
+    if (newUser) {
+      res.send({
+        _id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+        isAdmin: newUser.isAdmin,
+        token: getToken(newUser),
+      });
+    } else {
+      res.status(401).send({ message: 'Invalid User Data.' });
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  export default router;
